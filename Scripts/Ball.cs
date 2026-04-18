2bb74791b637193c13b772d8a98034118bc802cd
Jose Martinez <jdmartinez90@icloud.com>
2026-04-17 17:09:54 -0700

    fix: ensure ball has strong random horizontal velocity logic in Reset() - Force X Component to always be +-1 before adding random Y - Prevent near vertical movement that made rallies boring

--- Scripts/Ball.cs
+++ Scripts/Ball.cs
@@ -10,10 +10,13 @@ public partial class Ball : CharacterBody2D
         //move ball to center of screen
         Position = new Vector2(400, 300);
         
-        Vector2 direction = Vector2.Zero;
+        //xSign for x direction 
+        float xSign = (GD.Randf() >= 0.5) ? 1f : -1f;
+        
+        var direction = new Vector2(xSign, (float)GD.RandRange(-0.6, 0.6));
         
         //decide random  direction
-        direction = new Vector2(GD.Randf() * 2 - 1, (float)GD.RandRange(-0.6, 0.6));
+       // direction = new Vector2(GD.Randi() * 2 - 1, (float)GD.RandRange(-0.6, 0.6));
         direction = direction.Normalized() * Speed;
         
         //assign the vector to the ball
