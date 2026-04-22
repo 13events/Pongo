1bd0b24d6708c2bc269d5c6c78d23b891fe4b479
13events <20001872+13events@users.noreply.github.com>
2026-04-22 00:44:20 -0700

    feat: Implement goal zones, scoring, and HUD display (#1)
    - Add LeftGoal/RightGoal Area2D nodes with body_exited signals
    - Create goal zones to track player scoring
    - Implement HUD.cs with labels for leftScore and rightScore.
    - Create UpdateScore() in HUD.cs to update UI labels.
    - Update main.cs to cache ref to HUD
    - Update scoring logic to use HUD.UpdateScore()
    - Initialize UI score in main _Ready() to make sure score is displayed correctly at start of game

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -1,18 +1,25 @@
 using Godot;
 using System;
 
+
 public partial class Main : Node
 {
-	//get ball reference
+	//hold references for various nodes
 	private Ball _ball;
+	private Hud _hud;
+	
+	//variables track score
+	private int _leftScore = 0;
+	private int _rightScore = 0;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		 _ball =  GetNode<Ball>("Ball");
+		_hud = GetNode<Hud>("HUD");
+		 _ball =  GetNode<Ball>("Ball"); //get ref to ball
+		 _hud.UpdateScore(_leftScore, _rightScore);
 		 _ball.Reset();
-		 
-		 
+		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,4 +27,55 @@ public partial class Main : Node
 	{
 		
 	}
+	//check if ball exited left of screen, increment score for right paddle, reset ball.
+	public void OnLeftGoalBodyExited(Node2D body)
+	{
+		if (body is Ball) //checks and creats a variable in one line
+		{
+			GD.Print("Ball has exited the left goal");
+			
+			//Increment right paddle player score.
+			IncrementRightScore();
+			
+			//use HUD to update score
+			UpdateScore();
+			
+			_ball.Reset(); //reset ball
+		}
+	}
+	
+	//check if ball exited right of screen, increment score for left paddle, reset ball.
+	public void OnRightGoalBodyExited(Node2D body)
+	{
+		if (body is Ball) //checks and creats a variable in one line
+		{
+			GD.Print("Ball has exited the right goal");
+			
+			//Increment left paddle player score.
+			IncrementLeftScore();
+			
+			//use HUD to update score
+			UpdateScore();
+			
+			_ball.Reset();	//reset ball 
+		}
+	}
+
+	public void IncrementLeftScore()
+	{
+		_leftScore++;
+		GD.Print($"Left Player Scored! {_leftScore} - {_rightScore}");
+	}
+
+	public void IncrementRightScore()
+	{
+		_rightScore++;
+		GD.Print($"Right Player Scored! {_leftScore} - {_rightScore}");
+	}
+
+	public void UpdateScore()
+	{
+		_hud.UpdateScore(_leftScore, _rightScore);
+	}
+
 }
