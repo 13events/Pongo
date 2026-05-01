c351fed9732a50dd76fad7e94a8f702ae0fa5a4e
13events <jdmartinez90@icloud.com>
2026-05-01 00:58:34 -0700

    feat: Add AI logic for right Paddle.cs
    - Add export property bool isAI
    
    -Add ref variable for Ball and cache ref in _Ready()
    
    - _PhysicsProcess() now has an AI branch and Player Branch
    
    - Add healper method HandleAIMovement() containing AI paddle movment logic.

--- Scripts/Paddle.cs
+++ Scripts/Paddle.cs
@@ -6,6 +6,11 @@ public partial class Paddle : CharacterBody2D
 	
 	[Export]
 	public float Speed { get; set; }= 300.0f;	//speed paddles will move
+	
+	[Export]
+	public bool isAI {get; set;} = false;
+
+	private Ball _ball;
 
 	//Export Move actions so we can edit them in the Editor rather than using magic strings in code. 
 	[Export] 
@@ -13,27 +18,33 @@ public partial class Paddle : CharacterBody2D
 	
 	[Export] 
 	public string MoveDownAction { get; set; } = "move_down";
+
+	public override void _Ready()
+	{
+		_ball = GetNode<Ball>("../Ball"); //cache referece to ball.
+	}
 	
 	public override void _PhysicsProcess(double delta)
 	{
-		//debug movement
-		//GD.Print("PhysicsProcess running - direction = ", Input.GetAxis(MoveUpAction, MoveDownAction));
 		
-		//create new vector2 with the paddles' current Velocity
-		Vector2 velocity = Velocity;
+		if (isAI)
+		{
+			HandleAIMovement(delta);
+		}
+		else
+		{
+			//create new vector2 with the paddles' current Velocity
+			Vector2 velocity = Velocity;
 
-		//Calculate direction
-		float direction = Input.GetAxis(MoveUpAction, MoveDownAction);
-		
-		//set velocity.Y
-		velocity.Y = direction * Speed;
-		velocity.X = 0.0f;
-		
-		//Store value in Paddles' Velocity variable.
-		//Velocity = velocity;
-		
-		MoveAndCollide(velocity * (float)delta);
-		
+			//Calculate direction
+			float direction = Input.GetAxis(MoveUpAction, MoveDownAction);
+
+			//set velocity.Y
+			velocity.Y = direction * Speed;
+			velocity.X = 0.0f;
+			
+			MoveAndCollide(velocity * (float)delta);
+		}
 	}
 
 	public void Reset(Vector2 position)
@@ -49,5 +60,33 @@ public partial class Paddle : CharacterBody2D
 	public void ShowPaddle()
 	{
 		this.Visible = true;
+	}
+
+
+	public void HandleAIMovement(double delta)
+	{
+		var ballY = _ball.Position.Y;
+		var paddleY = Position.Y;
+		var distanceToBall = ballY - paddleY;
+		var paddleVelocity = Velocity;
+		
+		float direction;
+		
+		// compare and see if we are within 15pxl
+		if (Mathf.Abs(distanceToBall) <= 15f)
+		{
+			direction = 0f; 	//if ball is within 15px of paddle, stop movement
+		} 
+		else if (ballY > Position.Y)
+		{
+			direction = 1f;	//If we are farther than 15px and ball is below paddle, move down
+		}
+		else
+		{
+			direction = -1f;	//If we are farther than 15px and ball is above paddle, move up
+		}
+		
+		paddleVelocity.Y = direction * Speed * 0.7f;
+		MoveAndCollide(paddleVelocity * (float)delta);
 	}
 }
