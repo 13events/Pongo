2514815d3cb8ecc10cb3eaa3d99dec60ea87ede4
13events <20001872+13events@users.noreply.github.com>
2026-05-07 19:18:31 -0700

    Merge pull request #7
    * feat: Clean up UI navigation, create 1Player & 2Player selection opti…
    
    * feat: Clean up UI navigation, create 1Player & 2Player selection opti…
    
    * Merge remote-tracking branch 'origin/feature/menu-enhancements' into …
    
    * feat: add Windows 32-bit export preset and fix scene path reference

---
+++ Scenes/HowTo.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+public partial class HowTo : CanvasLayer
+{
+	private Button _menuButton;
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		_menuButton = GetNode<Button>("BackToMenuButton");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		
+	}
+
+	private void OnBackToMenuButtonPressed()
+	{
+		GetTree().ChangeSceneToFile("res://Scenes/MainMenu.tscn");
+	}
+}
